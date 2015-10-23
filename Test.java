dc4e22d1d9a111865d776d3b07925d8554bbfb2a
Joe Lam <joelam@MacBook.local>
2015-10-23 12:12:55 +0800

    finished move functions, reamin gameover function.

--- Test.java
+++ Test.java
@@ -2,59 +2,55 @@ public class Test {
 	public static void main(String[] args) {
 		ArrayQueue q = new ArrayQueue(5);
 
-		q.enqueue(1);
-		System.out.println("added: 1");
-		System.out.println(q.toString());
+		// for (int i = 1; i < 8; i++) {
+		// 	if (q.length() < 5) {
+		// 		q.enqueue(i);
+		// 		System.out.println("added: " + i);
+		// 		System.out.println("count: " + q.length());
+		// 		System.out.println("front: " + q.front());
+		// 		System.out.println("rear: " + q.rear());
+		// 		System.out.println(q.toString());
+		// 		System.out.println();
+		// 	} else {
+		// 		System.out.println("removed: " + q.dequeue());
+		// 		q.enqueue(i);
+		// 		System.out.println("added: " + i);
+		// 		System.out.println("count: " + q.length());
+		// 		System.out.println("front: " + q.front());	
+		// 		System.out.println("rear: " + q.rear());
+		// 		System.out.println(q.toString());
+		// 		System.out.println();
+		// 	}
+		// }
 
-		q.enqueue(2);
-		System.out.println("added: 2");
-		System.out.println(q.toString());
+		// // System.out.println("popped: " + q.pop());
+		// // System.out.println(q.toString());
 
-		q.enqueue(3);
-		System.out.println("added: 3");
-		System.out.println(q.toString());
+		// // System.out.println("removed: " + q.dequeue());
+		// // System.out.println(q.toString());
 
-		q.enqueue(4);
-		System.out.println("added: 4");
-		System.out.println(q.toString());
+		// // q.enqueue(9);
+		// // System.out.println("added: 9");
+		// // System.out.println(q.toString());
 
-		q.enqueue(5);
-		System.out.println("added: 5");
-		System.out.println(q.toString());
+		// // System.out.println("removed: " + q.dequeue());
+		// // System.out.println(q.toString());
 
-		System.out.println("removed: " + q.dequeue());
-		System.out.println(q.toString());
+		// // q.enqueue(10);
+		// // System.out.println("added: 10");
+		// // System.out.println(q.toString());
 
-		q.enqueue(6);
-		System.out.println("added: 6");
-		System.out.println(q.toString());
+		// // q.enqueue(11);
+		// // System.out.println("added: 11");
+		// // System.out.println(q.toString());
 
-		System.out.println("removed: " + q.dequeue());
-		System.out.println(q.toString());
-
-		q.enqueue(7);
-		System.out.println("added: 7");
-		System.out.println(q.toString());
-
-		System.out.println("popped: " + q.pop());
-		System.out.println(q.toString());
-
-		System.out.println("removed: " + q.dequeue());
-		System.out.println(q.toString());
-
-		q.enqueue(9);
-		System.out.println("added: 9");
-		System.out.println(q.toString());
-
-		System.out.println("removed: " + q.dequeue());
-		System.out.println(q.toString());
-
-		q.enqueue(10);
-		System.out.println("added: 10");
-		System.out.println(q.toString());
-
-		q.enqueue(11);
-		System.out.println("added: 11");
-		System.out.println(q.toString());
+		// while (q.length() > 0 ) {
+		// 	System.out.println("count: " + q.length());
+		// 	System.out.println("front: " + q.front());	
+		// 	System.out.println("rear: " + q.rear());
+		// 	System.out.println("popped: " + q.pop());
+		// 	System.out.println(q.toString());
+		// 	System.out.println();
+		// }
 	}
 }
